ab43ab2c3386b5a4b59b4ef3274ff71512ae1176
Pablo <pabloesquilache@gmail.com>
2023-02-02 19:49:57 -0300

    a lo de antes

--- javascript/estilo.js
+++ javascript/estilo.js
@@ -1,12 +1,12 @@
 /*FORMULARIO CONTACTO*/
 
-// const btnEnvio = document.getElementById('enviarCorreo')
+const btnEnvio = document.getElementById('enviarCorreo')
 
-// btnEnvio.addEventListener('click', function(e) {
-//     e.preventDefault();
-//     const nombre = document.getElementById("nombre").value;
-//     const email = document.getElementById("email").value;
-//     const asunto = document.getElementById("asunto").value;
-//     const mensaje = document.getElementById("mensaje").value;
-//     window.location.href=`mailto:pabloesquilache@gmail.com?subject=${asunto}&body=Nombre%20y%20Apellido%3A ${nombre}%0Aemail%3A ${email}%0AMensaje%3A ${mensaje}`;
-// });
+btnEnvio.addEventListener('click', function(e) {
+    e.preventDefault();
+    const nombre = document.getElementById("nombre").value;
+    const email = document.getElementById("email").value;
+    const asunto = document.getElementById("asunto").value;
+    const mensaje = document.getElementById("mensaje").value;
+    window.location.href=`mailto:pabloesquilache@gmail.com?subject=${asunto}&body=Nombre%20y%20Apellido%3A ${nombre}%0Aemail%3A ${email}%0AMensaje%3A ${mensaje}`;
+});
