4fae3d5f3949e8fa3d185144e1fb48d01ca640a4
Pablo <pabloesquilache@gmail.com>
2023-02-02 19:54:31 -0300

    con negrita

--- javascript/estilo.js
+++ javascript/estilo.js
@@ -8,5 +8,5 @@ btnEnvio.addEventListener('click', function(e) {
     const email = document.getElementById("email").value;
     const asunto = document.getElementById("asunto").value;
     const mensaje = document.getElementById("mensaje").value;
-    window.location.href=`mailto:pabloesquilache@gmail.com?subject=${asunto}&body=Nombre%20y%20Apellido%3A ${nombre}%0Aemail%3A ${email}%0AMensaje%3A ${mensaje}`;
+    window.location.href=`mailto:pabloesquilache@gmail.com?subject=${asunto}&body=<b>Nombre%20y%20Apellido</b>%3A ${nombre}%0A<b>email</b>%3A ${email}%0A<b>Mensaje</b>%3A ${mensaje}`;
 });
