b55b8556fac2f37b2711a95900aafe71918c96ce
Pablo <pabloesquilache@gmail.com>
2023-05-25 23:09:33 -0300

    a ver ahora si

--- javascript/contacto.js
+++ javascript/contacto.js
@@ -1,28 +1,3 @@
-// const btn = document.getElementById("button");
-
-// document.getElementById("form").addEventListener("submit", function (e) {
-//   e.preventDefault();
-
-//   btn.value = "Enviando...";
-
-//   const serviceID = "default_service";
-//   const templateID = "template_kym0z0o";
-
-//   emailjs.sendForm(serviceID, templateID, this).then(
-//     () => {
-//       btn.value = "Enviar";
-//       btn.value = "Mensaje enviado!";
-//       setTimeout(() => {
-//         btn.value = "Enviar";
-//         form.reset();
-//       },1000);
-//     },
-//     (err) => {
-//       btn.value = "Send Email";
-//       alert(JSON.stringify(err));
-//     }
-//   );
-// });
 
 const btn = document.getElementById("button");
 
