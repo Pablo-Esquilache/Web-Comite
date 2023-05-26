df1b5b6e0790000a0ef063c48b1dd3c9c741a4a8
Pablo <pabloesquilache@gmail.com>
2023-05-25 22:49:49 -0300

    a ver ahora

--- javascript/contacto.js
+++ javascript/contacto.js
@@ -1,3 +1,29 @@
+// const btn = document.getElementById("button");
+
+// document.getElementById("form").addEventListener("submit", function (e) {
+//   e.preventDefault();
+
+//   btn.value = "Enviando...";
+
+//   const serviceID = "default_service";
+//   const templateID = "template_kym0z0o";
+
+//   emailjs.sendForm(serviceID, templateID, this).then(
+//     () => {
+//       btn.value = "Enviar";
+//       btn.value = "Mensaje enviado!";
+//       setTimeout(() => {
+//         btn.value = "Enviar";
+//         form.reset();
+//       },1000);
+//     },
+//     (err) => {
+//       btn.value = "Send Email";
+//       alert(JSON.stringify(err));
+//     }
+//   );
+// });
+
 const btn = document.getElementById("button");
 
 document.getElementById("form").addEventListener("submit", function (e) {
@@ -12,8 +38,10 @@ document.getElementById("form").addEventListener("submit", function (e) {
     () => {
       btn.value = "Enviar";
       btn.value = "Mensaje enviado!";
-      form.reset();
-      btn.value = "Enviar";
+      setTimeout(() => {
+        btn.value = "Enviar";
+        form.reset();
+      }, 1000);
     },
     (err) => {
       btn.value = "Send Email";
